2dbfd64772597d4154ec71e9afd5d3ecdd437e6f
Neal H. Walfield <neal@sequoia-pgp.org>
2024-07-05 22:12:09 +0200

    Split the key lookup functionality out of key::delete.
      - Move the key lookup functionality in `common::key::delete` into a
        convenience function in `common::get_keys`.

--- src/common/key.rs
+++ src/common/key.rs
@@ -1,3 +1,18 @@
+use sequoia_openpgp as openpgp;
+use openpgp::Cert;
+use openpgp::KeyHandle;
+use openpgp::Result;
+use openpgp::cert::amalgamation::key::PrimaryKey;
+use openpgp::parse::Parse;
+use openpgp::packet::key;
+use openpgp::packet::Key;
+
+use sequoia_keystore as keystore;
+
+use crate::cli::types::FileStdinOrKeyHandle;
+use crate::Sq;
+
+
 mod expire;
 pub use expire::expire;
 
@@ -6,3 +21,136 @@ pub use delete::delete;
 
 pub mod password;
 pub use password::password;
+
+/// Looks up the certificate and returns the specified keys.
+///
+/// If `keys` is empty, all keys are returned.
+///
+/// If the keys are loaded from the certificate store, then a handle
+/// to the key is returned for each key.  Otherwise, the secret key is
+/// returned.
+///
+/// An error is returned if a key is not found, or there is no known
+/// secret key material.
+///
+/// The returned keys are not unlocked.
+pub fn get_keys<'a>(sq: &'a Sq,
+                    cert_handle: FileStdinOrKeyHandle,
+                    keys: Vec<KeyHandle>)
+    -> Result<(Cert, Vec<(Key<key::PublicParts, key::UnspecifiedRole>,
+                          bool,
+                          Option<Vec<keystore::Key>>)>)>
+{
+    let mut ks = None;
+
+    let cert = match cert_handle {
+        FileStdinOrKeyHandle::FileOrStdin(ref file) => {
+            let input = file.open()?;
+            let cert = Cert::from_buffered_reader(input)?;
+
+            // If it is not a TSK, there is nothing to do.
+            if ! cert.is_tsk() {
+                return Err(anyhow::anyhow!(
+                    "{} does not contain any secret key material.",
+                    cert.fingerprint()));
+            }
+
+            cert
+        }
+        FileStdinOrKeyHandle::KeyHandle(ref kh) => {
+            ks = Some(sq.key_store_or_else()?);
+            sq.lookup_one(kh, None, true)?
+        }
+    };
+
+    let mut ks = ks.map(|ks| ks.lock().unwrap());
+
+    let list: Vec<(Key<_, _>, bool, Option<_>)> = if keys.is_empty() {
+        // Get all secret key material.
+        let list: Vec<_>
+            = cert.keys().filter_map(|ka| {
+                if let Some(ks) = ks.as_mut() {
+                    let remote_keys = ks.find_key(ka.key_handle()).ok()?;
+                    if remote_keys.is_empty() {
+                        None
+                    } else {
+                        Some((ka.key().clone(), ka.primary(), Some(remote_keys)))
+                    }
+                } else {
+                    if ka.has_secret() {
+                        Some((ka.key().clone(), ka.primary(), None))
+                    } else {
+                        None
+                    }
+                }
+            }).collect();
+
+        // Make the primary last so that if something goes wrong it
+        // is still possible to generate a revocation certificate.
+        list.into_iter().rev().collect()
+    } else {
+        // Get only the specified secret key material.
+        let mut list = Vec::new();
+
+        let mut not_found_key_count = 0;
+        let mut no_secret_key_material_count = 0;
+        for key in keys.into_iter() {
+            let ka = if let Some(ka)
+                = cert.keys().find(|ka| ka.fingerprint().aliases(&key))
+            {
+                ka
+            } else {
+                eprintln!("{} does not contain {}",
+                          cert.fingerprint(), key);
+                not_found_key_count += 1;
+                continue;
+            };
+
+            let (no_secret_key_material, remote_keys)
+                = if let Some(ks) = ks.as_mut()
+            {
+                let remote_keys = ks.find_key(ka.key_handle())?;
+                (remote_keys.is_empty(), Some(remote_keys))
+            } else {
+                (! ka.has_secret(), None)
+            };
+
+            if no_secret_key_material {
+                eprintln!("{} does not contain any secret key material",
+                          key);
+                no_secret_key_material_count += 1;
+                continue;
+            }
+
+            list.push((ka.key().clone(), ka.primary(), remote_keys));
+        }
+
+        if not_found_key_count > 1 {
+            // Plural.
+            return Err(anyhow::anyhow!(
+                "{} keys not found", not_found_key_count));
+        } else if not_found_key_count > 0 {
+            // Singular.
+            return Err(anyhow::anyhow!(
+                "{} key not found", not_found_key_count));
+        }
+
+        if no_secret_key_material_count > 1 {
+            // Plural.
+            return Err(anyhow::anyhow!(
+                "{} of the specified keys don't have secret key material",
+                no_secret_key_material_count));
+        } else if no_secret_key_material_count > 0 {
+            // Singular.
+            return Err(anyhow::anyhow!(
+                "{} of the specified keys doesn't have secret key material",
+                no_secret_key_material_count));
+        }
+
+        list
+    };
+
+    assert!(! list.is_empty());
+
+    Ok((cert, list))
+}
--- src/common/key/delete.rs
+++ src/common/key/delete.rs
@@ -2,17 +2,16 @@
 use anyhow::Context;
 
 use sequoia_openpgp as openpgp;
-use openpgp::Cert;
 use openpgp::KeyHandle;
-use openpgp::Packet;
 use openpgp::Result;
-use openpgp::cert::amalgamation::key::PrimaryKey;
-use openpgp::parse::Parse;
+use openpgp::Packet;
 use openpgp::serialize::Serialize;
 
 use crate::cli::types::FileOrStdout;
 use crate::cli::types::FileStdinOrKeyHandle;
 use crate::Sq;
+
+use super::get_keys;
 
 pub fn delete(sq: Sq,
               cert_handle: FileStdinOrKeyHandle,
@@ -21,125 +20,18 @@ pub fn delete(sq: Sq,
               binary: bool)
     -> Result<()>
 {
-    let (cert, mut ks) = match cert_handle {
-        FileStdinOrKeyHandle::FileOrStdin(ref file) => {
-            let input = file.open()?;
-            let cert = Cert::from_buffered_reader(input)?;
+    let ks = matches!(cert_handle, FileStdinOrKeyHandle::KeyHandle(_));
 
-            // If it is not a TSK, there is nothing to strip.
-            if ! cert.is_tsk() {
-                return Err(anyhow::anyhow!(
-                    "{} does not contain any secret key material.",
-                    cert.fingerprint()));
-            }
+    let (cert, to_delete) = get_keys(&sq, cert_handle, keys)?;
 
-            (cert, None)
-        }
-        FileStdinOrKeyHandle::KeyHandle(ref kh) => {
-            let cert = sq.lookup_one(kh, None, true)?;
-
-            let ks = sq.key_store_or_else()?;
-            let ks = ks.lock().unwrap();
-
-            (cert, Some(ks))
-        }
-    };
-
-    let to_delete: Vec<(_, _)> = if keys.is_empty() {
-        // Delete all secret key material.
-        let to_delete: Vec<_>
-            = cert.keys().filter_map(|ka| {
-                if let Some(ks) = ks.as_mut() {
-                    let remote_keys = ks.find_key(ka.key_handle()).ok()?;
-                    if remote_keys.is_empty() {
-                        None
-                    } else {
-                        Some((ka, Some(remote_keys)))
-                    }
-                } else {
-                    if ka.has_secret() {
-                        Some((ka, None))
-                    } else {
-                        None
-                    }
-                }
-            }).collect();
-
-        // Delete the primary last so that if something goes wrong it
-        // is still possible to generate a revocation certificate.
-        to_delete.into_iter().rev().collect()
-    } else {
-        // Delete only the specified secret key material.
-        let mut to_delete = Vec::new();
-
-        let mut missing_key_count = 0;
-        let mut no_secret_key_material_count = 0;
-        for key in keys.into_iter() {
-            let ka = if let Some(ka)
-                = cert.keys().find(|ka| ka.fingerprint().aliases(&key))
-            {
-                ka
-            } else {
-                eprintln!("{} does not contain {}",
-                          cert.fingerprint(), key);
-                missing_key_count += 1;
-                continue;
-            };
-
-            let (no_secret_key_material, remote_keys)
-                = if let Some(ks) = ks.as_mut()
-            {
-                let remote_keys = ks.find_key(ka.key_handle())?;
-                (remote_keys.is_empty(), Some(remote_keys))
-            } else {
-                (! ka.has_secret(), None)
-            };
-
-            if no_secret_key_material {
-                eprintln!("{} does not contain any secret key material",
-                          key);
-                no_secret_key_material_count += 1;
-                continue;
-            }
-
-            to_delete.push((ka, remote_keys));
-        }
-
-        if missing_key_count > 1 {
-            // Plural.
-            return Err(anyhow::anyhow!(
-                "{} keys not found", missing_key_count));
-        } else if missing_key_count > 0 {
-            // Singular.
-            return Err(anyhow::anyhow!(
-                "{} key not found", missing_key_count));
-        }
-
-        if no_secret_key_material_count > 1 {
-            // Plural.
-            return Err(anyhow::anyhow!(
-                "{} of the keys to delete don't have secret key material",
-                no_secret_key_material_count));
-        } else if no_secret_key_material_count > 0 {
-            // Singular.
-            return Err(anyhow::anyhow!(
-                "{} of the keys to delete doesn't have secret key material",
-                no_secret_key_material_count));
-        }
-
-        to_delete
-    };
-
-    assert!(! to_delete.is_empty());
-
-    if ks.is_some() {
+    if ks {
         // Delete the secret key material from the key store.
-        for (ka, remote_keys) in to_delete.into_iter() {
+        for (key, _primary, remote_keys) in to_delete.into_iter() {
             let remote_keys = remote_keys.expect("have remote keys");
             assert!(! remote_keys.is_empty());
             for mut kh in remote_keys.into_iter() {
                 kh.delete_secret_key_material().with_context(|| {
-                    format!("Deleting {}", ka.fingerprint())
+                    format!("Deleting {}", key.fingerprint())
                 })?;
             }
         }
@@ -147,9 +39,9 @@ pub fn delete(sq: Sq,
         // Strip the secret key material from the certificate.
         let mut stripped: Vec<Packet> = Vec::new();
 
-        for (ka, _) in to_delete.into_iter() {
-            let pk = ka.key().clone().take_secret().0;
-            if ka.primary() {
+        for (key, primary, _) in to_delete.into_iter() {
+            let pk = key.take_secret().0;
+            if primary {
                 stripped.push(
                     Packet::PublicKey(pk.role_into_primary()));
             } else {

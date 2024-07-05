e35b87ec5b15ed13ec8c144d44e6721cde6d29a0
Neal H. Walfield <neal@sequoia-pgp.org>
2024-07-05 22:18:31 +0200

    Refactor key::password.
      - Refactor `common::key::password` to use the common key lookup
        functionality in `common::get_keys`.

--- src/common/key/password.rs
+++ src/common/key/password.rs
@@ -42,64 +42,114 @@ pub fn password(sq: Sq,
         Ok(new_password_.clone().unwrap())
     };
 
-    let from_file = match &cert_handle {
-        FileStdinOrKeyHandle::FileOrStdin(_file) => {
-            true
-        }
-        FileStdinOrKeyHandle::KeyHandle(_kh) => {
-            false
-        }
-    };
+    let ks = matches!(cert_handle, FileStdinOrKeyHandle::KeyHandle(_));
 
-    let cert = sq.lookup_one(cert_handle, None, true)?;
+    let (cert, mut list) = super::get_keys(&sq, cert_handle, vec![])?;
+    let uid = sq.best_userid(&cert, true);
 
-    if from_file {
+    if ks {
+        // Change the password of the secret key material on the key
+        // store.
+        for (key, _primary, remote_keys) in list.into_iter() {
+            let remote_keys = remote_keys.expect("have remote keys");
+            assert!(! remote_keys.is_empty());
+            for mut remote_key in remote_keys.into_iter() {
+                let provide_password
+                    = if let Protection::Password(hint) = remote_key.locked()?
+                {
+                    let mut unlocked = false;
+                    for p in sq.cached_passwords() {
+                        if remote_key.unlock(p).is_ok() {
+                            unlocked = true;
+                            break;
+                        }
+                    }
+
+                    if ! unlocked {
+                        if let Some(hint) = hint {
+                            eprintln!("{}", hint);
+                        }
+
+                        loop {
+                            let p = password::prompt_to_unlock(&format!(
+                                "{}/{}, {}",
+                                cert.keyid(), key.keyid(), uid))?;
+
+                            match remote_key.unlock(p.clone()) {
+                                Ok(()) => {
+                                    sq.cache_password(p.clone());
+                                    break;
+                                }
+                                Err(err) => {
+                                    eprintln!("Failed to unlock key: {}", err);
+                                }
+                            }
+                        }
+                    }
+                    true
+                } else {
+                    remote_key.password_source()?.is_inline()
+                };
+
+                let password = if provide_password {
+                    if let Some(password) = get_new_password()? {
+                        Some(password)
+                    } else {
+                        // change_password interprets None as prompt for
+                        // password, and "" as clear password.
+                        Some("".into())
+                    }
+                } else {
+                    None
+                };
+
+                remote_key.change_password(password.as_ref())
+                    .with_context(|| {
+                        format!("Changing {}'s password", key.fingerprint())
+                    })?;
+            }
+        }
+    } else {
         // First, decrypt all secrets.
-        let mut decrypted: Vec<Packet> = vec![
-            sq.decrypt_key(
+        for (key, _primary, _remote_keys) in list.iter_mut() {
+            *key = sq.decrypt_key(
                 Some(&cert),
-                cert.primary_key().key().clone().parts_into_secret()?,
+                key.clone().parts_into_secret()?,
                 true, // May prompt.
                 false, // Don't allow skipping.
-            )?.0.into(),
-        ];
-        for ka in cert.keys().subkeys().secret() {
-            decrypted.push(
-                sq.decrypt_key(
-                    Some(&cert),
-                    ka.key().clone().parts_into_secret()?,
-                    true, // May prompt.
-                    false, // Don't allow skipping.
-                )?.0.into(),
-            );
+            )?.0.parts_into_public();
         }
-        let mut cert = cert.insert_packets(decrypted)?;
-        assert_eq!(
-            cert.keys().secret().count(),
-            cert.keys().unencrypted_secret().count()
-        );
 
+        let mut packets: Vec<Packet> = Vec::new();
         if let Some(new) = get_new_password()? {
-            let mut encrypted: Vec<Packet> = vec![
-                cert
-                    .primary_key()
-                    .key()
-                    .clone()
-                    .parts_into_secret()?
+            for (key, primary, _remote_keys) in list.into_iter() {
+                let key = key.parts_into_secret()
+                    .expect("have secret key amterial")
                     .encrypt_secret(&new)?
-                    .into()
-            ];
-            for ka in cert.keys().subkeys().unencrypted_secret() {
-                encrypted.push(
-                    ka.key()
-                        .clone()
-                        .parts_into_secret()?
-                        .encrypt_secret(&new)?
-                        .into(),
-                );
+                    .parts_into_public();
+
+                if primary {
+                    packets.push(
+                        Packet::PublicKey(key.role_into_primary()));
+                } else {
+                    packets.push(
+                        Packet::PublicSubkey(key.role_into_subordinate()));
+                }
             }
-            cert = cert.insert_packets(encrypted)?;
+        } else {
+            for (key, primary, _remote_keys) in list.into_iter() {
+                let key = key.parts_into_public();
+                if primary {
+                    packets.push(
+                        Packet::PublicKey(key.role_into_primary()));
+                } else {
+                    packets.push(
+                        Packet::PublicSubkey(key.role_into_subordinate()));
+                }
+            }
         }
+
+        let cert = cert.insert_packets(packets)?;
 
         let output = output.unwrap_or_else(|| FileOrStdout::new(None));
         let mut output = output.for_secrets().create_safe(sq.force)?;
@@ -107,78 +157,6 @@ pub fn password(sq: Sq,
             cert.as_tsk().serialize(&mut output)?;
         } else {
             cert.as_tsk().armored().serialize(&mut output)?;
-        }
-    } else {
-        let vc = cert.with_policy(sq.policy, sq.time)?;
-
-        let uid = sq.best_userid(&cert, true);
-
-        let ks = sq.key_store_or_else()?;
-        let mut ks = ks.lock().unwrap();
-
-        for ka in vc.keys() {
-            let keys = ks.find_key(ka.key_handle())
-                .with_context(|| {
-                    format!("Looking up {}", ka.fingerprint())
-                })?;
-
-            // XXX: What should we do if the key is present multiple
-            // times?
-            let mut key = keys.into_iter().next().expect("have at least one");
-
-            let provide_password
-                = if let Protection::Password(hint) = key.locked()?
-            {
-                let mut unlocked = false;
-                for p in sq.cached_passwords() {
-                    if key.unlock(p).is_ok() {
-                        unlocked = true;
-                        break;
-                    }
-                }
-
-                if ! unlocked {
-                    if let Some(hint) = hint {
-                        eprintln!("{}", hint);
-                    }
-
-                    loop {
-                        let p = password::prompt_to_unlock(&format!(
-                            "{}/{}, {}",
-                            cert.keyid(), ka.keyid(), uid))?;
-
-                        match key.unlock(p.clone()) {
-                            Ok(()) => {
-                                sq.cache_password(p.clone());
-                                break;
-                            }
-                            Err(err) => {
-                                eprintln!("Failed to unlock key: {}", err);
-                            }
-                        }
-                    }
-                }
-                true
-            } else {
-                key.password_source()?.is_inline()
-            };
-
-            let password = if provide_password {
-                if let Some(password) = get_new_password()? {
-                    Some(password)
-                } else {
-                    // change_password interprets None as prompt for
-                    // password, and "" as clear password.
-                    Some("".into())
-                }
-            } else {
-                None
-            };
-
-            key.change_password(password.as_ref())
-                .with_context(|| {
-                    format!("Changing {}'s password", key.fingerprint())
-                })?;
         }
     }
 

658596b0ac6bbe68db4eedcdf95d95a93481bd2f
Justus Winter <justus@sequoia-pgp.org>
2024-08-14 10:30:24 +0200

    Port test to the common test framework.

--- tests/sq-key-generate.rs
+++ tests/sq-key-generate.rs
@@ -1,43 +1,27 @@
 use std::time;
 
-use assert_cmd::Command;
-use tempfile::TempDir;
-
 use sequoia_openpgp as openpgp;
 use openpgp::Result;
-use openpgp::cert::prelude::*;
-use openpgp::parse::Parse;
-use openpgp::policy::StandardPolicy;
+
+mod common;
 
 mod integration {
     use super::*;
 
-    const P: &StandardPolicy = &StandardPolicy::new();
-
     #[test]
     fn sq_key_generate_creation_time() -> Result<()>
     {
+        let sq = common::Sq::new();
+
         // $ date +'%Y%m%dT%H%M%S%z'; date +'%s'
         let iso8601 = "20220120T163236+0100";
         let t = 1642692756;
 
-        let dir = TempDir::new()?;
-        let key_pgp = dir.path().join("key.pgp");
-
-        // Build up the command line.
-        let mut cmd = Command::cargo_bin("sq")?;
-        cmd.args(["--no-cert-store",
-                  "--no-key-store",
-                  "key", "generate",
-                  "--no-userids",
-                  "--time", iso8601,
-                  "--expiration", "never",
-                  "--output", &*key_pgp.to_string_lossy()]);
-
-        cmd.assert().success();
-
-        let result = Cert::from_file(key_pgp)?;
-        let vc = result.with_policy(P, None)?;
+        let (result, _, _) = sq.key_generate(&[
+            "--time", iso8601,
+            "--expiration", "never",
+        ], &[]);
+        let vc = result.with_policy(common::STANDARD_POLICY, None)?;
 
         assert_eq!(vc.primary_key().creation_time(),
                    time::UNIX_EPOCH + time::Duration::new(t, 0));

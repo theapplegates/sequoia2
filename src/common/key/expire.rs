f6d62ab749dee995d98ce09d8bc6f9ee2b514027
Neal H. Walfield <neal@sequoia-pgp.org>
2024-10-08 10:35:35 +0200

    Normalize naming.
      - Make the code (and output) less confusing by normalizing names.

--- src/common/key/expire.rs
+++ src/common/key/expire.rs
@@ -20,59 +20,55 @@ use crate::Sq;
 use crate::sq::GetKeysOptions;
 
 pub fn expire(sq: Sq,
-              cert: FileStdinOrKeyHandle,
-              subkeys: &[KeyHandle],
+              cert_handle: FileStdinOrKeyHandle,
+              keys: &[KeyHandle],
               expiration: Expiration,
               mut output: Option<FileOrStdout>,
               binary: bool)
     -> Result<()>
 {
-    let policy = sq.policy.clone();
-
-    if cert.is_file() {
+    if cert_handle.is_file() {
         if output.is_none() {
             // None means to write to the cert store.  When reading
             // from a file, we want to write to stdout by default.
             output = Some(FileOrStdout::new(None));
         }
     }
-    let key = sq.lookup_one(cert, None, true)?;
-
-    let primary_handle = key.key_handle();
+    let cert = sq.lookup_one(cert_handle, None, true)?;
 
     let mut primary_signer
-        = sq.get_primary_key(&key, Some(&[GetKeysOptions::AllowNotAlive]))?;
+        = sq.get_primary_key(&cert, Some(&[GetKeysOptions::AllowNotAlive]))?;
+
+    let primary_handle = cert.key_handle();
+    let all_handles = cert.keys().map(|k| k.key_handle()).collect::<Vec<_>>();
 
     // Fix the new expiration time.
     let expiration_time = expiration.to_systemtime(sq.time);
 
-    // We want to check that all given key handles exist, make a list.
-    let handles = key.keys().map(|k| k.key_handle()).collect::<Vec<_>>();
-
     // We update the primary key if no subkey is given, or it is
-    // explicitly listed as subkey to change.
-    let mut update_primary_key = subkeys.is_empty();
+    // explicitly listed as a key to change.
+    let mut update_primary_key = keys.is_empty();
 
-    // We update the subkey bindings if they are explicitly given.
-    let update_subkeys = ! subkeys.is_empty();
+    // We only update subkey bindings if they are explicitly given.
+    let update_subkeys = ! keys.is_empty();
 
-    let mut keys = key.keys().subkeys();
-    for h in subkeys {
-        if ! handles.iter().any(|k| k.aliases(h)) {
-            wprintln!("Selected subkey {} does not exist in the key.", h);
+    let mut subkeys = cert.keys().subkeys();
+    for h in keys {
+        if ! all_handles.iter().any(|k| k.aliases(h)) {
+            wprintln!("Selected key {} does not exist in the certificate.", h);
             wprintln!();
-            wprintln!("The key has the following subkeys:");
+            wprintln!("The certificate has the following keys:");
             wprintln!();
-            for k in &handles {
+            for k in &all_handles {
                 wprintln!(" - {}", k);
             }
-            return Err(anyhow::anyhow!("selected subkey not found"));
+            return Err(anyhow::anyhow!("Selected key not found"));
         }
 
         if h.aliases(&primary_handle) {
             update_primary_key = true;
         } else {
-            keys = keys.key_handle(h.clone());
+            subkeys = subkeys.key_handle(h.clone());
         }
     }
 
@@ -82,44 +78,44 @@ pub fn expire(sq: Sq,
     if update_subkeys {
         // To update subkey expiration times, create new binding
         // signatures.
-        for skb in keys {
+        for ka in subkeys {
             // Preferably use the binding signature under our policy,
             // fall back to the most recent binding signature.
-            let template = skb.binding_signature(&policy, sq.time)
-                .or(skb.self_signatures().next()
+            let template = ka.binding_signature(sq.policy, sq.time)
+                .or(ka.self_signatures().next()
                     .ok_or(anyhow::anyhow!("no binding signature")))?
                 .clone();
 
             // Push a copy of the key to make reordering easier.
-            acc.push(Packet::from(skb.key().clone()));
-            acc.push(skb.bind(
+            acc.push(Packet::from(ka.key().clone()));
+            acc.push(ka.bind(
                 &mut primary_signer,
-                &key,
+                &cert,
                 SignatureBuilder::from(template)
                     .set_signature_creation_time(sq.time)?
-                    .set_key_expiration_time(skb.key(), expiration_time)?)?
+                    .set_key_expiration_time(ka.key(), expiration_time)?)?
                      .into());
         }
     }
 
-    // To change the key's expiration time, create a new direct key
+    // To change the cert's expiration time, create a new direct key
     // signature and new binding signatures for the user IDs.
     if update_primary_key {
         use openpgp::cert::amalgamation::ValidAmalgamation;
 
         let template =
         // Preferably use the direct key signature under our policy,
-            key.primary_key().binding_signature(&policy, sq.time).ok()
+            cert.primary_key().binding_signature(sq.policy, sq.time).ok()
         // fall back to the most recent direct key signature,
-            .or_else(|| key.primary_key().self_signatures().next())
+            .or_else(|| cert.primary_key().self_signatures().next())
         // fall back to the primary user ID's binding signature,
-            .or_else(|| key.with_policy(&policy, sq.time)
+            .or_else(|| cert.with_policy(sq.policy, sq.time)
                      .and_then(|vcert| vcert.primary_userid())
                      .map(|uidb| uidb.binding_signature())
                      .ok())
         // fall back to the newest user ID binding signature.
             .or_else(|| {
-                let mut sigs = key.userids()
+                let mut sigs = cert.userids()
                     .filter_map(|uidb| uidb.self_signatures().next())
                     .collect::<Vec<_>>();
                 sigs.sort_by_key(|s| s.signature_creation_time()
@@ -206,14 +202,14 @@ pub fn expire(sq: Sq,
         acc.push(template
                  .set_type(SignatureType::DirectKey)
                  .set_signature_creation_time(sq.time)?
-                 .set_key_expiration_time(key.primary_key().key(),
+                 .set_key_expiration_time(cert.primary_key().key(),
                                           expiration_time)?
                  .sign_direct_key(&mut primary_signer, None)?
                  .into());
 
-        for uidb in key.userids() {
+        for uidb in cert.userids() {
             if let RevocationStatus::Revoked(_)
-                = uidb.revocation_status(&policy, sq.time)
+                = uidb.revocation_status(sq.policy, sq.time)
             {
                 // The user ID is revoked.  Skip it.  (Adding a new
                 // self signature would actually "unrevoke it"!)
@@ -224,7 +220,7 @@ pub fn expire(sq: Sq,
             // policy as of the reference time.  If there is none,
             // fall back to the most recent binding signature.
             let template = if let Ok(sig) = uidb.binding_signature(
-                &policy, sq.time)
+                sq.policy, sq.time)
             {
                 sig.clone()
             } else if let Some(sig) = uidb.self_signatures().next() {
@@ -240,30 +236,30 @@ pub fn expire(sq: Sq,
             acc.push(Packet::from(uidb.userid().clone()));
             acc.push(uidb.bind(
                 &mut primary_signer,
-                &key,
+                &cert,
                 SignatureBuilder::from(template)
                     .set_signature_creation_time(sq.time)?
-                    .set_key_expiration_time(key.primary_key().key(),
+                    .set_key_expiration_time(cert.primary_key().key(),
                                              expiration_time)?)?
                      .into());
         }
     }
 
     // Merge and canonicalize.
-    let key = key.insert_packets(acc)?;
+    let cert = cert.insert_packets(acc)?;
 
     if let Some(sink) = output {
         let path = sink.path().map(Clone::clone);
         let mut output = sink.for_secrets().create_safe(&sq)?;
         if binary {
-            key.as_tsk().serialize(&mut output)?;
+            cert.as_tsk().serialize(&mut output)?;
         } else {
-            key.as_tsk().armored().serialize(&mut output)?;
+            cert.as_tsk().armored().serialize(&mut output)?;
         }
 
         if let Some(path) = path {
             sq.hint(format_args!(
-                "Updated key written to {}.  \
+                "Updated certificate written to {}.  \
                  To make the update effective, it has to be published \
                  so that others can find it, for example using:",
                 path.display()))
@@ -278,8 +274,8 @@ pub fn expire(sq: Sq,
     } else {
         let cert_store = sq.cert_store_or_else()?;
 
-        let keyid = key.keyid();
-        if let Err(err) = cert_store.update(Arc::new(key.into())) {
+        let keyid = cert.keyid();
+        if let Err(err) = cert_store.update(Arc::new(cert.into())) {
             wprintln!("Error importing updated cert: {}", err);
             return Err(err);
         } else {

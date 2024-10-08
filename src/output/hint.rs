a22f40410a113bc3430f239051ac74537613a586
Justus Winter <justus@sequoia-pgp.org>
2024-10-08 18:45:48 +0200

    Improve hint formatting.

--- src/output/hint.rs
+++ src/output/hint.rs
@@ -6,12 +6,18 @@ use std::fmt;
 pub struct Hint {
     /// Whether to suppress printing the hint.
     quiet: bool,
+
+    /// Whether this is the first hint in this hint block.
+    first: bool,
 }
 
 impl Hint {
     /// Constructs a new hint, optionally suppressing it.
     pub fn new(quiet: bool) -> Self {
-        Hint { quiet }
+        Hint {
+            quiet,
+            first: true,
+        }
     }
 
     /// Displays a message to the user.
@@ -19,11 +25,14 @@ impl Hint {
     /// It will be prefixed with "Hint: ", and should either end in a
     /// full stop or colon, depending on whether or not a command hint
     /// follows.
-    pub fn hint(self, msg: fmt::Arguments) -> Self {
+    pub fn hint(mut self, msg: fmt::Arguments) -> Self {
         if ! self.quiet {
-            wprintln!(initial_indent="Hint: ",
-                      subsequent_indent="      ",
-                      "{}", msg);
+            wprintln!();
+            wprintln!(
+                initial_indent=if self.first { "Hint: " } else { "      " },
+                subsequent_indent="      ",
+                "{}", msg);
+            self.first = false;
         }
         self
     }

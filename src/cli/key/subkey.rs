79defd3fdfec234cc01727f9818d0f0c0287f734
Neal H. Walfield <neal@sequoia-pgp.org>
2024-11-05 14:26:26 +0100

    Move cli parser for sq key subkey export to its own module.

--- src/cli/key/subkey.rs
+++ src/cli/key/subkey.rs
@@ -26,6 +26,7 @@ use crate::cli::types::KeyDesignators;
 use crate::cli::types::cert_designator;
 
 pub mod add;
+pub mod export;
 
 #[derive(Debug, Subcommand)]
 #[clap(
@@ -42,80 +43,12 @@ and revoke them.",
 #[non_exhaustive]
 pub enum Command {
     Add(add::Command),
-    Export(ExportCommand),
+    Export(export::Command),
     Delete(DeleteCommand),
     Password(PasswordCommand),
     Expire(ExpireCommand),
     Revoke(RevokeCommand),
     Bind(BindCommand),
-}
-
-const SUBKEY_EXPORT_EXAMPLES: Actions = Actions {
-    actions: &[
-        Action::Setup(Setup {
-            command: &[
-                "sq", "key", "import", "alice-secret.pgp",
-            ],
-        }),
-        Action::Example(Example {
-            comment: "\
-Export Alice's signing-capable and encryption-capable subkeys, but not \
-her primary key or her authentication-capable subkey.",
-            command: &[
-                "sq", "key", "subkey", "export",
-                "--key=42020B87D51877E5AF8D272124F3955B0B8DECC8",
-                "--key=74DCDEAF17D9B995679EB52BA6E65EA2C8497728",
-            ],
-        }),
-    ]
-};
-test_examples!(sq_subkey_key_export, SUBKEY_EXPORT_EXAMPLES);
-
-#[derive(Debug, Args)]
-#[clap(
-    about = "Export keys from the key store",
-    long_about = "
-Export secret key material from the secret key store.
-
-Exports the secret key material.  Note that even if secret key \
-material is available, it may not be exportable.  For instance, secret \
-key material stored on a hardware security module usually cannot be \
-exported from the device.
-
-If you want to export all secret key material associated with a \
-certificate, use `sq key export`.
-",
-    after_help = SUBKEY_EXPORT_EXAMPLES,
-)]
-pub struct ExportCommand {
-    #[clap(
-        long,
-        value_name = "FINGERPRINT|KEYID",
-        required = true,
-        help = "\
-Export the secret key material for the specified key, and its certificate",
-        long_help = "\
-Export the specified key.
-
-The entire certificate is exported, but only the specified key's \
-secret key material is exported.  An error is returned if the secret \
-key material for the specified key is not available.",
-    )]
-    pub key: Vec<KeyHandle>,
-
-    #[clap(
-        default_value_t = FileOrStdout::default(),
-        help = FileOrStdout::HELP_OPTIONAL,
-        long,
-        value_name = FileOrStdout::VALUE_NAME,
-    )]
-    pub output: FileOrStdout,
-
-    #[clap(
-        long,
-        help = "Emit binary data",
-    )]
-    pub binary: bool,
 }
 
 const SQ_KEY_SUBKEY_DELETE_EXAMPLES: Actions = Actions {
--- src/commands/key/subkey.rs
+++ src/commands/key/subkey.rs
@@ -23,7 +23,7 @@ use crate::cli::key::subkey::Command;
 use crate::cli::key::subkey::add::Command as AddCommand;
 use crate::cli::key::subkey::DeleteCommand;
 use crate::cli::key::subkey::ExpireCommand;
-use crate::cli::key::subkey::ExportCommand;
+use crate::cli::key::subkey::export::Command as ExportCommand;
 use crate::cli::key::subkey::PasswordCommand;
 use crate::cli::key::subkey::RevokeCommand;
 use crate::cli::types::EncryptPurpose;

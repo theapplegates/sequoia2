3824be8bbfe23ee64a577bdfe8f0924beec672d0
Neal H. Walfield <neal@sequoia-pgp.org>
2024-11-05 15:08:09 +0100

    Move the sq key subkey add implementation to its own module.

--- src/commands/key/subkey.rs
+++ src/commands/key/subkey.rs
@@ -1,16 +1,12 @@
 use std::collections::BTreeSet;
 
-use anyhow::Context;
-
 use chrono::DateTime;
 use chrono::Utc;
 
 use sequoia_openpgp as openpgp;
-use openpgp::cert::KeyBuilder;
 use openpgp::cert::SubkeyRevocationBuilder;
 use openpgp::packet::{Key, Signature, key};
 use openpgp::packet::signature::subpacket::NotationData;
-use openpgp::serialize::Serialize;
 use openpgp::types::KeyFlags;
 use openpgp::types::ReasonForRevocation;
 use openpgp::Cert;
@@ -20,15 +16,12 @@ use openpgp::Result;
 
 use crate::Sq;
 use crate::cli::key::subkey::Command;
-use crate::cli::key::subkey::add::Command as AddCommand;
 use crate::cli::key::subkey::delete::Command as DeleteCommand;
 use crate::cli::key::subkey::expire::Command as ExpireCommand;
 use crate::cli::key::subkey::export::Command as ExportCommand;
 use crate::cli::key::subkey::password::Command as PasswordCommand;
 use crate::cli::key::subkey::revoke::Command as RevokeCommand;
-use crate::cli::types::EncryptPurpose;
 use crate::commands::key::bind;
-use crate::common;
 use crate::common::key::expire;
 use crate::common::key::export;
 use crate::common::key::delete;
@@ -38,9 +31,11 @@ use crate::common::RevocationOutput;
 use crate::common::get_secret_signer;
 use crate::parse_notations;
 
+mod add;
+
 pub fn dispatch(sq: Sq, command: Command) -> Result<()> {
     match command {
-        Command::Add(c) => subkey_add(sq, c)?,
+        Command::Add(c) => add::dispatch(sq, c)?,
         Command::Export(c) => subkey_export(sq, c)?,
         Command::Delete(c) => subkey_delete(sq, c)?,
         Command::Password(c) => subkey_password(sq, c)?,
@@ -221,82 +216,6 @@ impl RevocationOutput for SubkeyRevocation {
     fn revoker(&self) -> &Cert {
         &self.revoker
     }
-}
-
-/// Add a new Subkey for an existing primary key
-///
-/// Creates a subkey with features (e.g. `KeyFlags`, `CipherSuite`) based on
-/// user input (or application-wide defaults if not specified).
-/// If no specific expiry is requested, the subkey never expires.
-fn subkey_add(
-    sq: Sq,
-    command: AddCommand,
-) -> Result<()> {
-    let cert =
-        sq.resolve_cert(&command.cert, sequoia_wot::FULLY_TRUSTED)?.0;
-
-    let valid_cert = cert.with_policy(sq.policy, sq.time)?;
-
-    let validity = command
-        .expiration
-        .as_duration(DateTime::<Utc>::from(sq.time))?;
-
-    let keyflags = KeyFlags::empty()
-        .set_authentication_to(command.can_authenticate)
-        .set_signing_to(command.can_sign)
-        .set_storage_encryption_to(matches!(
-            command.can_encrypt,
-            Some(EncryptPurpose::Storage) | Some(EncryptPurpose::Universal)
-        ))
-        .set_transport_encryption_to(matches!(
-            command.can_encrypt,
-            Some(EncryptPurpose::Transport) | Some(EncryptPurpose::Universal)
-        ));
-
-    // If a password is needed to use the key, the user will be prompted.
-    let (primary_key, password) =
-        match sq.get_primary_key(&cert, None) {
-            Ok(key) => {
-                // Don't use a password, or prompt for one.
-                if let Some(password_file) = command.new_password_file {
-                    let password = std::fs::read(&password_file)
-                        .with_context(|| {
-                            format!("Reading {}", password_file.display())
-                        })?;
-                    (key, Some(password.into()))
-                } else if command.without_password {
-                    (key, None)
-                } else {
-                    (key, common::password::prompt_for_new_or_none(
-                        &sq, "subkey")?)
-                }
-            }
-            Err(error) => {
-                return Err(error)
-            }
-        };
-
-    let new_cert = KeyBuilder::new(keyflags)
-        .set_creation_time(sq.time)
-        .set_cipher_suite(command.cipher_suite.as_ciphersuite())
-        .set_password(password)
-        .subkey(valid_cert)?
-        .set_key_validity_period(validity)?
-        .set_primary_key_signer(primary_key)
-        .attach_cert()?;
-
-    if let Some(output) = command.output {
-        let mut sink = output.for_secrets().create_safe(&sq)?;
-        if command.binary {
-            new_cert.as_tsk().serialize(&mut sink)?;
-        } else {
-            new_cert.as_tsk().armored().serialize(&mut sink)?;
-        }
-    } else {
-        sq.import_key(new_cert, &mut Default::default())?;
-    }
-
-    Ok(())
 }
 
 /// Revoke a Subkey of an existing primary key
---
+++ src/commands/key/subkey/add.rs
@@ -0,0 +1,89 @@
+use anyhow::Context;
+
+use chrono::DateTime;
+use chrono::Utc;
+
+use sequoia_openpgp as openpgp;
+use openpgp::Result;
+use openpgp::cert::KeyBuilder;
+use openpgp::serialize::Serialize;
+use openpgp::types::KeyFlags;
+
+use crate::Sq;
+use crate::cli::key::subkey::add::Command;
+use crate::cli::types::EncryptPurpose;
+use crate::common;
+
+/// Add a new Subkey for an existing primary key
+///
+/// Creates a subkey with features (e.g. `KeyFlags`, `CipherSuite`) based on
+/// user input (or application-wide defaults if not specified).
+/// If no specific expiry is requested, the subkey never expires.
+pub fn dispatch(sq: Sq, command: Command) -> Result<()>
+{
+    let cert =
+        sq.resolve_cert(&command.cert, sequoia_wot::FULLY_TRUSTED)?.0;
+
+    let valid_cert = cert.with_policy(sq.policy, sq.time)?;
+
+    let validity = command
+        .expiration
+        .as_duration(DateTime::<Utc>::from(sq.time))?;
+
+    let keyflags = KeyFlags::empty()
+        .set_authentication_to(command.can_authenticate)
+        .set_signing_to(command.can_sign)
+        .set_storage_encryption_to(matches!(
+            command.can_encrypt,
+            Some(EncryptPurpose::Storage) | Some(EncryptPurpose::Universal)
+        ))
+        .set_transport_encryption_to(matches!(
+            command.can_encrypt,
+            Some(EncryptPurpose::Transport) | Some(EncryptPurpose::Universal)
+        ));
+
+    // If a password is needed to use the key, the user will be prompted.
+    let (primary_key, password) =
+        match sq.get_primary_key(&cert, None) {
+            Ok(key) => {
+                // Don't use a password, or prompt for one.
+                if let Some(password_file) = command.new_password_file {
+                    let password = std::fs::read(&password_file)
+                        .with_context(|| {
+                            format!("Reading {}", password_file.display())
+                        })?;
+                    (key, Some(password.into()))
+                } else if command.without_password {
+                    (key, None)
+                } else {
+                    (key, common::password::prompt_for_new_or_none(
+                        &sq, "subkey")?)
+                }
+            }
+            Err(error) => {
+                return Err(error)
+            }
+        };
+
+    let new_cert = KeyBuilder::new(keyflags)
+        .set_creation_time(sq.time)
+        .set_cipher_suite(command.cipher_suite.as_ciphersuite())
+        .set_password(password)
+        .subkey(valid_cert)?
+        .set_key_validity_period(validity)?
+        .set_primary_key_signer(primary_key)
+        .attach_cert()?;
+
+    if let Some(output) = command.output {
+        let mut sink = output.for_secrets().create_safe(&sq)?;
+        if command.binary {
+            new_cert.as_tsk().serialize(&mut sink)?;
+        } else {
+            new_cert.as_tsk().armored().serialize(&mut sink)?;
+        }
+    } else {
+        sq.import_key(new_cert, &mut Default::default())?;
+    }
+
+    Ok(())
+}

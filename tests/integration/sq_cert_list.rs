theapplegates/sequoia2

commit 8483a01f60ff496c1670d62938aae81931c71ec7

Rework sq cert list's human readable output.
  - Rewrite `sq cert list` to improve the human readable output.  In
    particular, lint the input when we are unable to satisfy a query,
    and improve the error messages.  Also, when matching user IDs,
    don't show bindings that can't be authenticated.

  - To accomplish this, we revise `authenticate`'s interface from
    using a complicated, ad-hoc specification of queries, to using a
    first class representation.  This representation better preserves
    the user's intent.

src/commands/cert.rs
@@ -10,7 +10,8 @@ use crate::{
     Sq,
     Result,
     cli::cert::{Command, list, Subcommands},
-    cli::types::cert_designator,
+    commands::cert::authenticate::Query,
+    commands::cert::authenticate::QueryKind,
     common::pki::authenticate,
 };
 
@@ -29,11 +30,13 @@ pub fn dispatch(sq: Sq, command: Command) -> Result<()>
 
         // List all authenticated bindings.
         Subcommands::List(list::Command {
-            mut certs, pattern, gossip, certification_network, trust_amount,
+            certs, pattern, gossip, certification_network, trust_amount,
             show_paths,
         }) => {
-            if let Some(pattern) = pattern {
-                let mut d = None;
+            let mut certs: Vec<Query> = Query::for_cert_designators(certs);
+
+            if let Some(pattern) = pattern.as_ref() {
+                let mut query_kind = None;
                 if let Ok(kh) = pattern.parse::<KeyHandle>() {
                     if matches!(kh, KeyHandle::Fingerprint(Fingerprint::Invalid(_))) {
                         let hex = pattern.chars()
@@ -44,34 +47,30 @@ pub fn dispatch(sq: Sq, command: Command) -> Result<()>
 
                         if hex >= 16 {
                             weprintln!("Warning: {} looks like a fingerprint or key ID, \
-                                        but its invalid.  Treating it as a text pattern.",
+                                        but it is invalid.  Treating it as a text pattern.",
                                        pattern);
                         }
                     } else {
-                        d = Some(cert_designator::CertDesignator::Cert(kh));
+                        query_kind = Some(QueryKind::Cert(kh));
                     }
                 };
 
-                certs.push(d.unwrap_or_else(|| {
-                    cert_designator::CertDesignator::Grep(pattern)
-                }));
+                let query_kind = query_kind.unwrap_or_else(|| {
+                    QueryKind::Pattern(pattern.clone())
+                });
+                certs.push(Query {
+                    argument: Some(format!("{:?}", pattern)),
+                    kind: query_kind,
+                });
             }
-
-            let certs = sq.resolve_certs_or_fail(
-                &certs,
-                if *gossip {
-                    0
-                } else {
-                    trust_amount.map(|t| t.amount())
-                        .unwrap_or(sequoia_wot::FULLY_TRUSTED)
-                })?;
 
             authenticate(
                 &mut std::io::stdout(),
-                &sq, certs.is_empty(), None,
-                *gossip, *certification_network, *trust_amount,
-                None, None,
-                (! certs.is_empty()).then_some(certs),
+                &sq,
+                certs,
+                *gossip,
+                *certification_network,
+                *trust_amount,
                 *show_paths)
         },
 

src/commands/download.rs
@@ -41,6 +41,7 @@ use crate::commands::network::USER_AGENT;
 use crate::commands::verify::verify;
 use crate::common::ui;
 use crate::common::pki::authenticate;
+use crate::common::pki::authenticate::Query;
 
 // So we can deal with either named temp files or files.
 enum SomeFile {
@@ -399,14 +400,13 @@ pub fn dispatch(sq: Sq, c: download::Command)
                                     let good = authenticate(
                                         &mut std::io::stderr(),
                                         &sq,
-                                        false, // precompute
-                                        None, // list pattern
+                                        vec![
+                                            Query::for_key_handle(
+                                                None, cert.key_handle())
+                                        ],
                                         false, // gossip
                                         false, // certification network
                                         Some(TrustAmount::Full), // trust amount
-                                        None, // user ID
-                                        Some(&cert),
-                                        None,
                                         true, // show paths
                                     ).is_ok();
 

src/commands/pki.rs
@@ -11,6 +11,7 @@ use crate::cli;
 
 use crate::Sq;
 use crate::common::pki::authenticate;
+use crate::commands::pki::authenticate::Query;
 
 pub fn dispatch(sq: Sq, cli: cli::pki::Command, matches: &ArgMatches)
                 -> Result<()>
@@ -25,16 +26,17 @@ pub fn dispatch(sq: Sq, cli: cli::pki::Command, matches: &ArgMatches)
             userid, gossip, certification_network, trust_amount,
             cert, show_paths,
         }) => {
-            let cert = sq.resolve_cert(&cert, 0)?.0;
-
+            assert_eq!(cert.len(), 1);
             assert_eq!(userid.len(), 1);
-            let userid = userid.designators.into_iter().next().unwrap();
 
             authenticate(
                 &mut std::io::stdout(),
-                &sq, false, None,
-                *gossip, *certification_network, *trust_amount,
-                Some(&userid), Some(&cert), None, *show_paths,
+                &sq,
+                Query::for_binding(cert, userid),
+                *gossip,
+                *certification_network,
+                *trust_amount,
+                *show_paths,
             )?
         }
 
@@ -45,13 +47,15 @@ pub fn dispatch(sq: Sq, cli: cli::pki::Command, matches: &ArgMatches)
             userid, show_paths,
         }) => {
             assert_eq!(userid.len(), 1);
-            let userid = userid.designators.into_iter().next().unwrap();
 
             authenticate(
                 &mut std::io::stdout(),
-                &sq, false, None,
-                *gossip, *certification_network, *trust_amount,
-                Some(&userid), None, None, *show_paths)?;
+                &sq,
+                userid.into(),
+                *gossip,
+                *certification_network,
+                *trust_amount,
+                *show_paths)?;
         }
 
         // Find and list all authenticated bindings for a given
@@ -60,13 +64,16 @@ pub fn dispatch(sq: Sq, cli: cli::pki::Command, matches: &ArgMatches)
             gossip, certification_network, trust_amount,
             cert, show_paths,
         }) => {
-            let cert = sq.resolve_cert(&cert, 0)?.0;
+            assert_eq!(cert.len(), 1);
 
             authenticate(
                 &mut std::io::stdout(),
-                &sq, false, None,
-                *gossip, *certification_network, *trust_amount,
-                None, Some(&cert), None, *show_paths)?;
+                &sq,
+                cert.into(),
+                *gossip,
+                *certification_network,
+                *trust_amount,
+                *show_paths)?;
         }
 
         // Authenticates a given path.

src/common/pki.rs
@@ -1,4 +1,4 @@
-mod authenticate;
+pub mod authenticate;
 pub use authenticate::authenticate;
 pub use authenticate::required_trust_amount;
 

tests/integration/sq_pki.rs
@@ -1334,7 +1334,7 @@ fn list_pattern() -> Result<()> {
         .map(|(userid, target)| {
             (1, format!("- {} {}", HR_OK, userid).to_string())
         })
-        .chain(vec![(3, HR_OK.to_string())].into_iter())
+        .chain(vec![(bindings.len(), HR_OK.to_string())].into_iter())
         .collect::<Vec<_>>();
 
     test(

commit 8f7eb6fd60e19640af6d9e4e00b6c744ded9907f

Fix listing certificates by fingerprint or key ID.
  - The recent (post 1.0.0) commit
    8483a01f60ff496c1670d62938aae81931c71ec7 broke `sq cert list
    --cert KEY_HANDLE`.  `sq cert list --cert KEY_HANDLE` should
    return success if the certificate is valid, even if no user IDs
    can be authenticated.

  - This commit restores the behavior of sq 1.0.0 with the following
    exception:

    - When using `sq cert list --cert KEY_HANDLE`, if the certificate
      is valid, but all bindings are considered invalid according to
      the cryptographic policy, or are revoked, sq 1.0.0 considers
      the certificate as not authenticated, and fails.

    - This commit fixes `sq cert list --cert KEY_HANDLE` to succeed,
      since the certificate itself is valid.

    - Note: sq 1.0.0 correctly succeeds when the certificate has no
      bindings.

  - Fixes #510.

src/commands/cert.rs
@@ -33,7 +33,7 @@ pub fn dispatch(sq: Sq, command: Command) -> Result<()>
             certs, pattern, gossip, certification_network, trust_amount,
             show_paths,
         }) => {
-            let mut certs: Vec<Query> = Query::for_cert_designators(certs);
+            let mut certs: Vec<Query> = Query::for_cert_designators(certs, true);
 
             if let Some(pattern) = pattern.as_ref() {
                 let mut query_kind = None;
@@ -51,7 +51,7 @@ pub fn dispatch(sq: Sq, command: Command) -> Result<()>
                                        pattern);
                         }
                     } else {
-                        query_kind = Some(QueryKind::Cert(kh));
+                        query_kind = Some(QueryKind::AuthenticatedCert(kh));
                     }
                 };
 

src/common/pki/authenticate.rs
@@ -37,6 +37,7 @@ const TRACE: bool = false;
 
 /// The different kinds of queries that we support.
 pub enum QueryKind {
+    AuthenticatedCert(KeyHandle),
     Cert(KeyHandle),
     UserID(String),
     Email(String),
@@ -53,7 +54,9 @@ impl QueryKind {
         use QueryKind::*;
 
         match self {
-            Cert(kh) | UserIDBinding(kh, _) | EmailBinding(kh, _) => {
+            AuthenticatedCert(kh) | Cert(kh)
+                | UserIDBinding(kh, _) | EmailBinding(kh, _) =>
+            {
                 Some(kh.clone())
             }
             _ => None
@@ -116,7 +119,8 @@ impl Query {
 
     /// Converts a set of certificate designators to a set of queries.
     pub fn for_cert_designators<Arguments, Prefix, Options, Doc>(
-        designators: CertDesignators<Arguments, Prefix, Options, Doc>)
+        designators: CertDesignators<Arguments, Prefix, Options, Doc>,
+        certs_are_authenticated: bool)
         -> Vec<Query>
     where
         Arguments: typenum::Unsigned,
@@ -139,7 +143,11 @@ impl Query {
                         unreachable!("Not allowed in this context");
                     }
                     Cert(kh) => {
-                        QueryKind::Cert(kh.clone())
+                        if certs_are_authenticated {
+                            QueryKind::AuthenticatedCert(kh.clone())
+                        } else {
+                            QueryKind::Cert(kh.clone())
+                        }
                     }
                     UserID(userid) => QueryKind::UserID(userid.clone()),
                     Email(email) => QueryKind::Email(email.clone()),
@@ -230,7 +238,7 @@ where
     fn from(designators: CertDesignators<Arguments, Prefix, Options, Doc>)
         -> Vec<Query>
     {
-        Query::for_cert_designators(designators)
+        Query::for_cert_designators(designators, false)
     }
 }
 
@@ -376,8 +384,11 @@ where 'store: 'rstore,
 
     for (i, query) in queries.iter().enumerate() {
         match &query.kind {
-            QueryKind::Cert(kh) => {
+            QueryKind::AuthenticatedCert(kh) | QueryKind::Cert(kh) => {
                 t!("Authenticating {}", kh);
+
+                let cert_authenticated
+                    = matches!(query.kind, QueryKind::AuthenticatedCert(_));
 
                 for fpr in resolve_key_handle(kh) {
                     let count = bindings.len();
@@ -386,13 +397,13 @@ where 'store: 'rstore,
                             .into_iter()
                             .map(|userid| {
                                 (fpr.clone(), Some(userid),
-                                 true, vec![ i ])
+                                 cert_authenticated, vec![ i ])
                             }));
 
                     if bindings.len() == count {
                         // No user IDs.  Add the certificate.
                         bindings.push((fpr.clone(), None,
-                                       true, vec![ i ]));
+                                       cert_authenticated, vec![ i ]));
                     }
                 }
             }
@@ -597,8 +608,35 @@ where 'store: 'rstore,
     let mut output = ConciseHumanReadableOutputNetwork::new(
         o, &sq, required_amount, show_paths);
 
-    for (fingerprint, userid, always_show, i) in bindings.iter() {
-        let authenticated = if let Some(userid) = userid {
+    // Look up the certificate, and return it if it is valid.
+    let check_cert = |fpr: &Fingerprint| -> Result<Cert> {
+        // Look up the certificate.
+        let certs = cert_store.lookup_by_cert(&KeyHandle::from(fpr))?;
+        assert_eq!(certs.len(), 1, "there can be only one");
+
+        let lc = certs.into_iter().next().unwrap();
+
+        let cert = lc.to_cert()?;
+
+        // Check if the certificate is valid according to the current
+        // policy.
+        let vc = cert.with_policy(sq.policy, sq.time)?;
+
+        // Check if the certificate is live.
+        let _ = vc.alive()?;
+
+        // Check that it is not revoked.
+        if let RevocationStatus::Revoked(_)
+            = cert.revocation_status(sq.policy, sq.time)
+        {
+            return Err(anyhow::anyhow!("{} is revoked", cert.fingerprint()));
+        }
+
+        Ok(cert.clone())
+    };
+
+    for (fingerprint, userid, cert_authenticated, i) in bindings.iter() {
+        if let Some(userid) = userid {
             let paths = if gossip {
                 n.gossip(fingerprint.clone(), userid.clone())
             } else {
@@ -608,16 +646,29 @@ where 'store: 'rstore,
 
             let aggregated_amount = paths.amount();
             t!("{}, {:?}: {}", fingerprint, userid, aggregated_amount);
-            let authenticated = if aggregated_amount >= required_amount {
+            let userid_authenticated = if aggregated_amount >= required_amount {
                 // We authenticated the binding!
                 true
-            } else if gossip {
-                // We're in gossip mode, show everything.
+            } else if gossip && aggregated_amount > 0 {
+                // We're in gossip mode, show all bindings...
                 true
-            } else if *always_show {
-                // We're authenticating a certificate, which was specified
-                // explicitly.  We don't consider it authenticated, but we
-                // do want to show it.
+            } else if gossip && aggregated_amount == 0 {
+                // ... as long as the certificate is valid.
+                match check_cert(&fingerprint) {
+                    Err(err) => {
+                        t!("Skipping {}: {}", fingerprint, err);
+                        continue;
+                    }
+                    Ok(_cert) => true,
+                }
+            } else if *cert_authenticated {
+                // The binding is not authenticated, but we should
+                // show the certificate if it is valid.
+                if let Err(err) = check_cert(&fingerprint) {
+                    t!("Skipping {}: {}", fingerprint, err);
+                    continue;
+                }
+
                 false
             } else {
                 // Don't show it.
@@ -625,25 +676,26 @@ where 'store: 'rstore,
                 continue;
             };
 
-            let paths = paths.into_iter().collect::<Vec<(wot::Path, usize)>>();
-
             output.add_cert(fingerprint)?;
-            output.add_paths(paths, fingerprint, userid, aggregated_amount)?;
+            if userid_authenticated {
+                let paths = paths.into_iter().collect::<Vec<(wot::Path, usize)>>();
+                output.add_paths(paths, fingerprint, userid, aggregated_amount)?;
 
-            authenticated
+                bindings_shown += 1;
+            }
         } else {
             // A cert without bindings.
-            output.add_cert(fingerprint)?;
+            if let Err(err) = check_cert(fingerprint) {
+                t!("Skipping {}: {}", fingerprint, err);
+                continue;
+            }
 
-            true
+            output.add_cert(fingerprint)?;
+            bindings_shown += 1;
         };
 
-        bindings_shown += 1;
-
-        if authenticated {
-            for i in i.into_iter() {
-                queries_satisfied[*i] = true;
-            }
+        for i in i.into_iter() {
+            queries_satisfied[*i] = true;
         }
     }
 
@@ -660,7 +712,7 @@ where 'store: 'rstore,
         let query = &queries[i];
 
         if gossip {
-            weprintln!("No bindings match {}.",
+            weprintln!("No valid bindings match {}.",
                        query.argument.as_deref().unwrap_or("the query"));
         } else {
             weprintln!("No bindings matching {} could be authenticated.",
@@ -791,7 +843,7 @@ where 'store: 'rstore,
     if bindings.is_empty() {
         // There are no matching bindings.
 
-        weprintln!("No bindings match.");
+        weprintln!("No bindings match the query.");
 
         if queries.len() == 1 {
             if let QueryKind::Pattern(pattern) = &queries[0].kind {
@@ -832,12 +884,14 @@ where 'store: 'rstore,
     } else if gossip {
         // We are in gossip mode.  Mention `sq pki link` as a way to
         // mark bindings as authenticated.
-        if ! bindings.is_empty() {
+        if bindings_shown > 0 {
             weprintln!("After checking that a user ID really belongs to \
                         a certificate, use `sq pki link add` to mark \
                         the binding as authenticated, or use \
                         `sq network search FINGERPRINT|EMAIL` to look for \
                         new certifications.");
+        } else {
+            weprintln!("No bindings are valid.");
         }
     } else if bindings.len() - bindings_shown > 0 {
         // Some of the matching bindings were not shown.  Tell the

tests/data/keys/expired-cert.pgp
@@ -1,0 +1,44 @@
+-----BEGIN PGP PRIVATE KEY BLOCK-----
+Comment: 7365 0426 9A55 BADA F707  48EF 6CAC BA3F 826C 427D
+Comment: expired <expired@example.org>
+
+xVgEX+O4lRYJKwYBBAHaRw8BAQdAEW29t6ZothGNfQP+VkRSom0wh/t1q/xaW689
+rcXBT1MAAQCScABGL7EaE89ZcWDOrk6BHB1YPxPwTolAZXFVUw7jKwwewsARBB8W
+CgCDBYJf47iVBYkFpI+9AwsJBwkQbKy6P4JsQn1HFAAAAAAAHgAgc2FsdEBub3Rh
+dGlvbnMuc2VxdW9pYS1wZ3Aub3JnrawE7FC0CD1a/uPsFs9pHCNnSmAYclNZ3N5x
+2E4ViWQDFQoIApsBAh4BFiEEc2UEJppVutr3B0jvbKy6P4JsQn0AAL4eAQDkRo5U
+b/JV6vkBLHtGi3YZE1oUh8QIhLot/nbmpIEV4gEAth/WkXp3vv2VgfHgKQuReGph
+elFo9K16J0Op+B8EXAvNHWV4cGlyZWQgPGV4cGlyZWRAZXhhbXBsZS5vcmc+wsAU
+BBMWCgCGBYJf47iVBYkFpI+9AwsJBwkQbKy6P4JsQn1HFAAAAAAAHgAgc2FsdEBu
+b3RhdGlvbnMuc2VxdW9pYS1wZ3Aub3Jnm92kUwQqGX6ix7GHpY3GGE3xSrmVxaLF
+4SCXDJvk3t0DFQoIApkBApsBAh4BFiEEc2UEJppVutr3B0jvbKy6P4JsQn0AAHWE
+AP9c0hZMP2M00l+13JxLI1HgH/mvpeXX8zjNy/n3HPgwnwD/QowP0uaK1UnOnk7E
+Ldc7zkW/K/22FHIJw+6Q1dZNYADHWARf47iVFgkrBgEEAdpHDwEBB0AwGGAkFeCm
+OZgDcvOSh/lGxWrqRu4PLBBwc/QDuVU9GAAA/33tmqLcancwaJ8uEEK6DGaE84/r
+Xsid5Lb5U6KrdjSOEmTCwMUEGBYKATcFgl/juJUFiQWkj70JEGysuj+CbEJ9RxQA
+AAAAAB4AIHNhbHRAbm90YXRpb25zLnNlcXVvaWEtcGdwLm9yZwcQvw9KnhM2t+DK
+E8jWmLMc9jeYSA0yjPMQbabvnow3ApsCvqAEGRYKAG8Fgl/juJUJEO/j9VwF13hc
+RxQAAAAAAB4AIHNhbHRAbm90YXRpb25zLnNlcXVvaWEtcGdwLm9yZ4pTMTEVp1kJ
+GOEwuuOfaXnJhm9ZFwHDk4YpkDf3EUNDFiEEIz0KXk51jH85cRAN7+P1XAXXeFwA
+AIjgAP49WA1XfsxyQYTYy9jArKddK9ipyJ86Lik72h41bU3ougEAihMmwXqVCDSa
+2gdfKBugOBDq7iQEXdQxKAeRgi61JAsWIQRzZQQmmlW62vcHSO9srLo/gmxCfQAA
+OscBAKcHcThryWUu/ULwO6u8m0Kom6TOFHtKlsG7bEfl4gj7AQDsNyY4Gin7/6zO
+F3fJ3E5AdJGiCz6XcTbrfuldrgHWC8dYBF/juJUWCSsGAQQB2kcPAQEHQFSeoQ4N
+48YyWX0gyyws8fD/SHOyPQIBLe49otj/roTKAAEA9ab0l/eDqZ/tIPP/4UrOLZ/n
+knVc+kPA4HAWl03SqS0T5sLAxQQYFgoBNwWCX+O4lQWJBaSPvQkQbKy6P4JsQn1H
+FAAAAAAAHgAgc2FsdEBub3RhdGlvbnMuc2VxdW9pYS1wZ3Aub3JnFOdE3EBtlmEA
+3uW8sX3fWg4WzeB0HNck84uHEMovEMACmyC+oAQZFgoAbwWCX+O4lQkQbubec1zl
+7SJHFAAAAAAAHgAgc2FsdEBub3RhdGlvbnMuc2VxdW9pYS1wZ3Aub3JnVAOEaAnC
+CLAZ4am57IDK/k7Khm3LCPGTLuwvApy+0fUWIQTd+qutH6uhMgIfrtpu5t5zXOXt
+IgAA+3cA/i5BdDk/mtOoDxOQm3UV3f3+HQ6XXvSv4jp4sI+mSzKuAP0di+8ySlH7
+q9c4wFajX3KViL2prjrqdDeJEdpPrSg6ABYhBHNlBCaaVbra9wdI72ysuj+CbEJ9
+AACA3wEA1AJXJQlJuu4I3dCzkaXSvtqkxIexqjAbxAes/0TWZLgA/38+3Ja21vB2
+SVD/kGx6avvRav7OsOYTArfrSVSNb8cFx10EX+O4lRIKKwYBBAGXVQEFAQEHQCCh
+rRG3PQibuABoN5nv27lGI2U9wxAiYJnXCduxsSMpAwEIBwAA/18Z+98FXN9wzvLz
+56e/0WaABxLf0azFsftGsenAs3ZAFKfCwAYEGBYKAHgFgl/juJUFiQWkj70JEGys
+uj+CbEJ9RxQAAAAAAB4AIHNhbHRAbm90YXRpb25zLnNlcXVvaWEtcGdwLm9yZ7yH
+PlSowzS44LN7rRRxHhTOiK+P5XofVK9yRzHXHfj3ApsMFiEEc2UEJppVutr3B0jv
+bKy6P4JsQn0AAN5HAP0cLZLR3YE5ADlZaS+m8E2VeCOWeLrIl4eBw8jYB3bX8wD6
+A/xLL+KP7T/wIwO+f+NAwmP4mWTr/NZlPXxJCM/iJAc=
+=6f2O
+-----END PGP PRIVATE KEY BLOCK-----

tests/data/keys/soft-revoked-cert.pgp
@@ -1,0 +1,45 @@
+-----BEGIN PGP PRIVATE KEY BLOCK-----
+
+xVgEZ0DnyhYJKwYBBAHaRw8BAQdAk+JMP/6spDFPwdgXgNV6tAUMo/rCKVcY8b+V
+gXwJ2asAAQCa3xa4BRnvaBr5HoteVWY5mXGkGGUBkY9KURR/ihqkCgyXwsADBCAW
+CgB1BYJnQOfeCRA48J/nH2+QSUcUAAAAAAAeACBzYWx0QG5vdGF0aW9ucy5zZXF1
+b2lhLXBncC5vcmfsvLCBcAsAmoMAh8MP5l1OaxROsZMX6IUvzUaklARolwUdA2J5
+ZRYhBGCM84Gkc3BQiNd+CDjwn+cfb5BJAAADSAEAjs/Qt6GcTnBsBG+L4e61q2Rk
+PzP6qBDJRzO2K05BFLAA/1s+cIDYrcNNVB8KhgaI8X1LXbXCE8QuEBkZduB7dY0J
+wsALBB8WCgB9BYJnQOfKAwsJBwkQOPCf5x9vkElHFAAAAAAAHgAgc2FsdEBub3Rh
+dGlvbnMuc2VxdW9pYS1wZ3Aub3JnUFN9aRUKDJsy8orMHOBkUuEM4j05f+wmw27G
+M/H4IzoDFQoIApsBAh4BFiEEYIzzgaRzcFCI134IOPCf5x9vkEkAABBjAP98GSqQ
+2B7zd1fUk+TsPn0pIYg2Ffp1tV3Nzz/1Uf9ylgD7BZOVn4qjMsenvzc71BRqVxjP
+9eXcgok2BGrr2LYNWgfNB3Jldm9rZWTCwA4EExYKAIAFgmdA58oDCwkHCRA48J/n
+H2+QSUcUAAAAAAAeACBzYWx0QG5vdGF0aW9ucy5zZXF1b2lhLXBncC5vcmck4xET
+U73HKwtH2cMy30mQFQDKhYNfTgqRMWBKZEfO+QMVCggCmQECmwECHgEWIQRgjPOB
+pHNwUIjXfgg48J/nH2+QSQAAo4oA/j9n1fJhA0yrLzxqxpRvXf2ZbfCwTx6BLYw3
+A+xNcJYJAQDYIlJ2EFuihNVQW33Axz78ayAuI7LoeL0tmpa2GJQWCsdYBGdA58oW
+CSsGAQQB2kcPAQEHQDzrmGOUAR2rjmmL8NFwnuQxy22GcKRn9prtASbgkUKPAAD/
+QbIzPB45OyKnJB68r0G1UziGFglsSGlPyMC93Zpzb4IOG8LAvwQYFgoBMQWCZ0Dn
+ygkQOPCf5x9vkElHFAAAAAAAHgAgc2FsdEBub3RhdGlvbnMuc2VxdW9pYS1wZ3Au
+b3JnK9EwRycNTwMyeCbBm3tiedHISBu1enLKdfqaS+QOhFECmyC+oAQZFgoAbwWC
+Z0DnygkQZwdeWY8kDAFHFAAAAAAAHgAgc2FsdEBub3RhdGlvbnMuc2VxdW9pYS1w
+Z3Aub3Jn6SBq+8GI8rJ9MoB4NSvevovn8E/eu/c0ZST1qSLkA7oWIQROOM5zJPiA
+tyYrAx9nB15ZjyQMAQAAH38A/2MUTtVAzwYv4fkaL6cWgaOlVYqT+nvS4pzSb0Oq
+W5X7AQDs+NmUaa391pn0iMWrPcQ2k1+m2HF/l9VxP84WQttdCBYhBGCM84Gkc3BQ
+iNd+CDjwn+cfb5BJAAA85wD/SEJcbfCzijELFiOlj56P+y6HrR8rxM7fOCSCSJ8n
+VmMBAODXaAmwD/wor3kEw7Rhhzy0mKS6bL5D+x9ZaFIwPhUPx1gEZ0DnyhYJKwYB
+BAHaRw8BAQdAd1ny9tacuYHl4vr1jAOYLUJj9Yucqtj9rGgOXtj6u/0AAQDov7aB
+mkoesnlY0QT33SPN6QNYNvFuVySHuBfsjr1bDhBHwsC/BBgWCgExBYJnQOfKCRA4
+8J/nH2+QSUcUAAAAAAAeACBzYWx0QG5vdGF0aW9ucy5zZXF1b2lhLXBncC5vcmeQ
+sLbALzvo9vzAKNC/PjW/k2GJZ321Q7Z0XJ0/FJAflAKbAr6gBBkWCgBvBYJnQOfK
+CRALouc4U225QkcUAAAAAAAeACBzYWx0QG5vdGF0aW9ucy5zZXF1b2lhLXBncC5v
+cme/sgI/zDxH116ECbOMBuCibmxkWVStQT3De+AWJbX1rhYhBGqk9VojTvwd09oU
+rAui5zhTbblCAADVwAEA6lcj2EPgyynxWW/hCMSya6DLsOzD82SjJ7LqNStRYeYA
+/iOdKtolI5WXiem5Jr1JoOy9ThrBENbK9pLkGzJubn0NFiEEYIzzgaRzcFCI134I
+OPCf5x9vkEkAALDgAQClnblj77GHDHbfHLxncnHvwIHaUPEspYP3ltE7yJzPBwEA
+8lWmAGyvxSifYMoOocl1KNwQpedn9difR2xuIZyHTQPHXQRnQOfKEgorBgEEAZdV
+AQUBAQdA+Wi/WCR5FxtDCnK9NKjSgTKT2IjVXviIGBHMce/4GWYDAQgHAAD/RP6g
+kl0JM5Zszxsssd9UsfY7dcgGxCPzLqyB76AdldARc8LAAAQYFgoAcgWCZ0DnygkQ
+OPCf5x9vkElHFAAAAAAAHgAgc2FsdEBub3RhdGlvbnMuc2VxdW9pYS1wZ3Aub3Jn
+6LU8O/fQ813OOOrkdLnicT53lcUlCMvICIcwk4KFXxgCmwwWIQRgjPOBpHNwUIjX
+fgg48J/nH2+QSQAAdn0A/338MqwhOUpaTkyy5txUQZvoeF/DRU0yI1uBzyJEGjWe
+AQDAn6J14VQYeLw2J0HgfvI26LbY5Ph2TS6CXTNnqaT6AQ==
+=E6M6
+-----END PGP PRIVATE KEY BLOCK-----

tests/integration/sq_cert_list.rs
@@ -1,4 +1,11 @@
+use sequoia_openpgp as openpgp;
+use openpgp::Cert;
+use openpgp::cert::amalgamation::ValidateAmalgamation;
+use openpgp::parse::Parse;
+
+use super::common::artifact;
 use super::common::Sq;
+use super::common::STANDARD_POLICY;
 use super::common::UserIDArg;
 
 #[test]
@@ -296,4 +303,119 @@ fn list_no_userids() {
 
     let output = sq.cert_list(&["--gossip", &fp]);
     assert!(std::str::from_utf8(&output).unwrap().contains(&fp));
+}
+
+/// Check that --cert FPR shows certificates that are otherwise
+/// unauthenticate.
+#[test]
+fn list_unauthenticated_cert() {
+    let sq = Sq::new();
+
+    let email = "alice@example.org";
+    let name = "Alice Lovelace";
+    let userid = &format!("{} <{}>", name, email);
+    let (cert, cert_path, _rev_path)
+        = sq.key_generate(&[], &[ userid ]);
+
+    sq.key_import(&cert_path);
+
+    // The certificate is unauthenticated so the following won't work:
+    assert!(sq.cert_list_maybe(&[email]).is_err());
+
+    // If we use --cert FINGERPRINT or provide the fingerprint, it
+    // will.
+    sq.cert_list(&[&cert.fingerprint().to_string()]);
+    sq.cert_list(&["--cert", &cert.fingerprint().to_string()]);
+
+    // When we link it, the above will work.
+    sq.pki_link_add(&[], cert.key_handle(), &[userid]);
+    sq.cert_list(&[email]);
+    sq.cert_list(&[&cert.fingerprint().to_string()]);
+    sq.cert_list(&["--cert", &cert.fingerprint().to_string()]);
+}
+
+#[test]
+fn list_invalid_certs() {
+    // Check that we can't list invalid certificates.
+    //
+    // Check:
+    //
+    // - a certificate that only uses SHA-1
+    // - a certificate that is revoked
+    // - a certificate that is expired
+    for path in [
+        "keys/only-sha1-priv.pgp",
+        "keys/soft-revoked-cert.pgp",
+        "keys/expired-cert.pgp",
+    ] {
+        eprintln!("Checking {}", path);
+
+        let sq = Sq::new();
+
+        let cert_file = artifact(path);
+        let cert = Cert::from_file(&cert_file).expect("valid cert");
+        let fpr = &cert.fingerprint().to_string()[..];
+        let userids = cert.userids()
+            .map(|ua| String::from_utf8_lossy(ua.userid().value()))
+            .collect::<Vec<_>>();
+
+        sq.cert_import(&cert_file);
+
+        assert!(sq.cert_list_maybe(&[fpr]).is_err());
+        assert!(sq.cert_list_maybe(&["--gossip", fpr]).is_err());
+
+        for userid in userids.iter() {
+            assert!(sq.cert_list_maybe(&["--userid", &userid[..]]).is_err());
+            assert!(sq.cert_list_maybe(&["--gossip", "--userid", &userid[..]])
+                    .is_err());
+        }
+    }
+}
+
+#[test]
+fn list_sha1_userid() {
+    // Check that we can list a user ID with --gossip even if it is
+    // only bound by a self-signature that relies on SHA-1.
+    let sq = Sq::new();
+
+    let cert_file = artifact("keys/sha1-userid-priv.pgp");
+    let cert = Cert::from_file(&cert_file).expect("valid cert");
+    let fpr = &cert.fingerprint().to_string()[..];
+
+    sq.cert_import(&cert_file);
+
+    // Listing the certificate is okay.
+    sq.cert_list(&[fpr]);
+    sq.cert_list(&["--gossip", fpr]);
+
+    let mut saw_invalid = false;
+
+    for ua in cert.userids() {
+        let userid = String::from_utf8_lossy(ua.userid().value());
+
+        if ua.with_policy(STANDARD_POLICY, None).is_err() {
+            saw_invalid = true;
+        }
+
+        // Not linked, so should fail.
+        assert!(
+            sq.cert_list_maybe(&["--cert-userid", &userid[..]]).is_err());
+        // Using --gossip should succeed.
+        sq.cert_list(&["--gossip", "--cert-userid", &userid[..]]);
+    }
+
+    assert!(saw_invalid);
+
+    // We now link all of the user IDs.  Even though the
+    // self-signature relies on SHA-1, the links don't so we'll be
+    // able to authenticate the user IDs.
+    for ua in cert.userids() {
+        let userid = String::from_utf8_lossy(ua.userid().value()).to_string();
+        sq.pki_link_add(&[], cert.key_handle(), &[UserIDArg::AddUserID(&userid)]);
+
+        assert!(
+            sq.cert_list_maybe(&["--cert-userid", &userid[..]]).is_ok());
+        assert!(
+            sq.cert_list_maybe(&["--gossip", "--cert-userid", &userid[..]]).is_ok());
+    }
 }
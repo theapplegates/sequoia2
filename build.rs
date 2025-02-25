0a9d5df4863cd197775997baa54b3a2c4b63842e
Neal H. Walfield <neal@sequoia-pgp.org>
2025-02-25 15:33:17 +0100

    Split out the man page generation into a new crate.
      - Split out the man page generation into a new crate, `sequoia-man`.
    
      - Use it.

--- build.rs
+++ build.rs
@@ -1,18 +1,14 @@
-use std::env;
-use std::fs;
-use std::path::PathBuf;
 use clap::ValueEnum;
 use clap_complete::Shell;
 use anyhow::Result;
+
+use sequoia_man::asset_out_dir;
+use sequoia_man::generate_man_pages;
 
 pub mod cli {
     #![allow(unused_macros)]
     include!("src/macros.rs");
     include!("src/cli/mod.rs");
-}
-
-pub mod man {
-    include!("src/man.rs");
 }
 
 fn main() {
@@ -25,28 +21,13 @@ fn main() {
 
     let mut sq = cli::build(false);
     generate_shell_completions(&mut sq).unwrap();
-    generate_man_pages(&sq).unwrap();
+
+    let version = env!("CARGO_PKG_VERSION");
+
+    let man_pages = asset_out_dir("man-pages").unwrap();
+    generate_man_pages(&man_pages, &sq, version, None).unwrap();
+
     lint_help_texts(&sq).unwrap();
-}
-
-/// Variable name to control the asset out directory with.
-const ASSET_OUT_DIR: &str = "ASSET_OUT_DIR";
-
-/// Returns the directory to write the given assets to.
-fn asset_out_dir(asset: &str) -> Result<PathBuf> {
-    println!("cargo:rerun-if-env-changed={}", ASSET_OUT_DIR);
-    let outdir: PathBuf =
-        env::var_os(ASSET_OUT_DIR).unwrap_or_else(
-            || env::var_os("OUT_DIR").expect("OUT_DIR not set")).into();
-    if outdir.exists() && ! outdir.is_dir() {
-        return Err(
-            anyhow::anyhow!("{}={:?} is not a directory",
-                            ASSET_OUT_DIR, outdir));
-    }
-
-    let path = outdir.join(asset);
-    fs::create_dir_all(&path)?;
-    Ok(path)
 }
 
 /// Generates shell completions.
@@ -58,19 +39,6 @@ fn generate_shell_completions(sq: &mut clap::Command) -> Result<()> {
     };
 
     println!("cargo:warning=shell completions written to {}", path.display());
-    Ok(())
-}
-
-/// Generates man pages.
-fn generate_man_pages(sq: &clap::Command) -> Result<()> {
-    let path = asset_out_dir("man-pages")?;
-
-    for man in man::manpages(sq) {
-        std::fs::write(path.join(man.filename()), man.troff_source())?;
-    }
-
-    println!("cargo:warning=man pages written to {}", path.display());
-
     Ok(())
 }
 
